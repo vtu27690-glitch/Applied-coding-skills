7489942e4caf5897d4da5d0ea5b75f702934679c
vtu27690-glitch <vtu27690@veltech.edu.in>
2026-08-13 09:07:50 +0530

    Add sortedSquares method for squared array
    Implement a solution to return the squares of a sorted array in non-decreasing order.

---
+++ week 1 /session 1  /Squares of a sorted array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int left = 0;
+        int right = n - 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                result[i] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[i] = nums[right] * nums[right];
+                right--;
+            }
+        }
+
+        return result;
+    }
+} 
+
+Input
+nums =
+[-4,-1,0,3,10]
+Output
+[0,1,9,16,100]
+Expected
+[0,1,9,16,100]
