8dc348830e56e8eb73f1548699c764aa61d68f6b
vtu27690-glitch <vtu27690@veltech.edu.in>
2026-08-27 09:31:31 +0530

    Add solution for minimum removals to valid parentheses
    Implemented a solution to remove invalid parentheses from a string to make it valid.

---
+++ week3 /task24 /minimum remove to make valid parentheses.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        Stack<Integer> stack = new Stack<>();
+        boolean[] remove = new boolean[s.length()];
+
+        // Find invalid parentheses
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                stack.push(i);
+            } else if (ch == ')') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                } else {
+                    remove[i] = true;
+                }
+            }
+        }
+
+        // Remaining '(' are invalid
+        while (!stack.isEmpty()) {
+            remove[stack.pop()] = true;
+        }
+
+        // Build the valid string
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!remove[i]) {
+                result.append(s.charAt(i));
+            }
+        }
+
+        return result.toString();
+    }
+}
+
+Input
+s =
+"lee(t(c)o)de)"
+Output
+"lee(t(c)o)de"
+Expected
+"lee(t(c)o)de"
