0d00ac78cfb4b9ac763fa427dd78e829b5b7f50b
vtu27690-glitch <vtu27690@veltech.edu.in>
2026-08-27 09:16:00 +0530

    Add StockSpanner class with next method
    Implement StockSpanner class to calculate stock span.

---
+++ week3 /task21 /online stock span.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class StockSpanner {
+
+    Stack<int[]> stack;
+
+    public StockSpanner() {
+        stack = new Stack<>();
+    }
+
+    public int next(int price) {
+
+        int span = 1;
+
+        // Remove previous prices <= current price
+        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+            span += stack.pop()[1];
+        }
+
+        // Store price and its span
+        stack.push(new int[]{price, span});
+
+        return span;
+    }
+}
+
+Input
+["StockSpanner","next","next","next","next","next","next","next"]
+[[],[100],[80],[60],[70],[60],[75],[85]]
+Output
+[null,1,1,1,2,1,4,6]
+Expected
+[null,1,1,1,2,1,4,6]
