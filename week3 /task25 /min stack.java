0a5a625d1563672ae7dd5667f356e1aa87533fe9
vtu27690-glitch <vtu27690@veltech.edu.in>
2026-08-27 09:34:46 +0530

    Add MinStack class with min tracking functionality
    Implemented a MinStack class that supports push, pop, top, and getMin operations.

---
+++ week3 /task25 /min stack.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class MinStack {
+
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+
+        if (minStack.isEmpty()) {
+            minStack.push(val);
+        } else {
+            minStack.push(Math.min(val, minStack.peek()));
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+Output
+[null,null,null,null,-3,null,0,-2]
+Expected
+[null,null,null,null,-3,null,0,-2]
